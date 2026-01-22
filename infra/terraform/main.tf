a951955c844873fbe096f38bd1d25225b3887232
NagaSekhar23 <72132465+NagaSekhar23@users.noreply.github.com>
2026-01-21 21:53:49 -0600

    feat: add base manifests, RBAC, terraform infra

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,38 +1,39 @@
 terraform {
+  required_version = ">= 1.3"
   required_providers {
-    aws = { source = "hashicorp/aws", version = "~> 5" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.40"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.11"
+    }
   }
 }
 
-provider "aws" { region = "us-east-1" }
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
-  name = "gitops-vpc"
-  cidr = "10.0.0.0/16"
-  azs = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
-  enable_nat_gateway = true
-  single_nat_gateway = true
+provider "aws" {
+  region = var.aws_region
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
-  cluster_name = "gitops-eks"
-  cluster_version = "1.31"
-  vpc_id = module.vpc.vpc_id
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.30"
+
+  vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
+
   eks_managed_node_groups = {
-    workers = {
-      min_size = 1
-      max_size = 3
-      desired_size = 1
-      instance_types = ["t3.small"]
+    platform = {
+      min_size     = 2
+      max_size     = 10
+      desired_size = 3
+
+      instance_types = ["t3.large"]
+      capacity_type  = "ON_DEMAND"
     }
   }
-  enable_irsa = true
 }
-
